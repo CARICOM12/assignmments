2333b5770bc47dbc63627ec764da98be00b731b9
CARICOM12 <0190a@ukr.net>
2020-05-03 15:27:26 +0300

    Add files via upload

---
+++ assignmments/assignmments/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace assignmments
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int x, y, z;
+            x = y = z = 100; // set x, y, and z to 100
+
+            x = x - 100;
+            x -= 100;
+
+            x = x + 100;
+            x += 100;
+
+            x = x * 100;
+            x *= 100;
+
+            x = x / 100;
+            x /= 100;
+
+            x = x & 100;
+            x &= 100;
+
+
+            |= ^=
+
+            x %= 100;
+            x = x % 100;
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
